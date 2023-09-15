524d5569a9a2f17478c409cdb1919daa493628a9
Vikkans <Jakobsenviktor6@gmail.com>
2023-09-15 12:25:50 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            string x = Console.ReadLine();
+            Console.WriteLine("Så din favorit bok är " + x);
+            Console.ReadKey();
+        }
+    }
+
+
+}
